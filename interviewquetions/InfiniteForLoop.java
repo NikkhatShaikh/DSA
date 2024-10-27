819aabc85cab6dadbb36968c811f896bed190a9f
shaikh nikkhat <nikkhat.shaikh@caprium.com>
2024-10-27 11:58:14 +0530

    infiniteForLoop

---
+++ interviewquetions/InfiniteForLoop.java
@@ -0,0 +1,18 @@
+package interviewquetions;
+
+public class InfiniteForLoop {
+    public static void main(String[] args) {
+//
+//        for (int i=0;i<=0;i--){
+//            System.out.println("Nikkhat");
+//        }
+
+//        for(int i=0;i>=0;i++){
+//            System.out.println("Nikkhat");
+//        }
+
+        for (int i=0;;i++){
+            System.out.println("Nikkhat");
+        }
+    }
+}
